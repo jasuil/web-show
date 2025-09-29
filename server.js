70dfebad746f1759840eeabcf3dc4410497f6e9b
Jang Sungil <jasuil@daum.net>
2025-09-30 00:03:53 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+// Node.js + Express + socket.io 서버
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // 모든 클라이언트에 전송
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
